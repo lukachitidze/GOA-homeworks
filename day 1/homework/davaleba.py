ed053b9654e9bde6be816b92c5139a1c441fd88f
lukachitidze <qweqwe@gmail.comluka.chitidze2109@gmail.com>
2024-06-01 14:29:55 +0400

    second commit

---
+++ day 1/homework/davaleba.py
@@ -0,0 +1,10 @@
+name = "luka"
+lastname = "chitidze"
+age = "13"
+hobby = "programming"
+country = "georgia"
+print(name + " " + lastname + " " + age + " " + hobby + " " + country)
+print(9 + 0)
+print(9 * 6)
+print(9 // 3)
+print(9 - 8)
