40fa8e185ab1c23ec3ca311f0943af310c3da3a7
lukachitidze <qweqwe@gmail.comluka.chitidze2109@gmail.com>
2024-06-02 11:32:48 +0400

    third commit

---
+++ day 2/classwork/classwork.py
@@ -0,0 +1,5 @@
+message = "game over"
+print(message)
+price = 139
+print("price:")
+print(price)
---
+++ day 2/homework/homework.py
@@ -0,0 +1,19 @@
+#magalitebi
+print("magalitebi:")
+print(5 + 5)
+print(6 + 7)
+print(9 - 8)
+print(80 - 15)
+print(6 * 9)
+print(7 * 1)
+print(15 / 3)
+print(60 / 20)
+#cvladebi
+name = " luka"
+lastname = " chitidze"
+age = " 13 years old"
+hobby = " coding"
+country = " georgia"
+print(name + lastname + age + hobby + country)
+
+
